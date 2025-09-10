70bd2cecea508045f337befc57bf0c045e47f0c1
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-09-10 23:52:55 +0600

    remove all previous code

--- lib/flutter_unified_image_picker.dart
+++ lib/flutter_unified_image_picker.dart
@@ -1,8 +1,1 @@
 
-import 'flutter_unified_image_picker_platform_interface.dart';
-
-class FlutterUnifiedImagePicker {
-  Future<String?> getPlatformVersion() {
-    return FlutterUnifiedImagePickerPlatform.instance.getPlatformVersion();
-  }
-}

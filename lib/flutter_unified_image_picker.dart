47a39421225aa0e1dbd887578b876dd7da551ccd
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-09-11 00:34:19 +0600

    add file path

--- lib/flutter_unified_image_picker.dart
+++ lib/flutter_unified_image_picker.dart
@@ -1,1 +1,7 @@
+// ignore_for_file: unnecessary_library_name
 
+library flutter_unified_image_picker;
+
+export 'src/controller/image_picker_controller.dart';
+export 'src/views/camera_view.dart';
+export 'src/views/draggable_sheet_widget.dart';
